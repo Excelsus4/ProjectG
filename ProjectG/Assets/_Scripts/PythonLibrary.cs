e125eeafa3a965224ac4dc4109183ec3029c7977
Excelsus4 <elijahko4@gmail.com>
2020-08-10 11:53:44 +0900

    IronPython 2.7.9 임포트
    그리고 간단한 파이선 스크립트를 호출하고 그 결과를 출력해봄

---
+++ ProjectG/Assets/_Scripts/PythonLibrary.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using IronPython.Hosting;
+
+namespace Exodrifter.UnityPython.Examples
+{
+	public class PythonLibrary : MonoBehaviour
+	{
+		void Start()
+		{
+			var engine = Python.CreateEngine();
+			var scope = engine.CreateScope();
+
+			// Add the python library path to the engine. Note that this will
+			// not work for builds; you will need to manually place the python
+			// library files in a place that your code can find it at runtime.
+			var paths = engine.GetSearchPaths();
+			paths.Add(Application.dataPath + "/Python/Lib");
+			engine.SetSearchPaths (paths);
+
+			string code = @"
+import os
+filename = os.path.abspath ('PythonScripts/helloWorld.py');";
+
+			var source = engine.CreateScriptSourceFromString(code);
+			source.Execute(scope);
+
+			dynamic py = engine.ExecuteFile(scope.GetVariable<string>("filename"));
+			dynamic pytest = py.Test("by_excelsus");
+			Debug.Log(pytest.display());
+		}
+	}
+}
---
+++ ProjectG/PythonScripts/helloWorld.py
@@ -0,0 +1,5 @@
+class Test():
+    def __init__(self, name):
+        self.name = name
+    def display(self):
+        return "Hi, " + self.name
