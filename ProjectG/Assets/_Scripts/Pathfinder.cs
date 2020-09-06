3edee8659a48ff687cf95fe65d928da9198be5c0
Excelsus4 <elijahko4@gmail.com>
2020-09-06 15:52:08 +0900

    샘플 캐릭터와 애니메이션: Ch44

--- ProjectG/Assets/_Scripts/Pathfinder.cs
+++ ProjectG/Assets/_Scripts/Pathfinder.cs
@@ -7,26 +7,33 @@ public class Pathfinder : MonoBehaviour
 	public Vector3 EndLocation;
 	public float MoveSpeed;
 	public int Interval;
+	private Animator animator;
 	private int CurrentNode;
 	private List<Vector3> route;
 	private DataConverter finder;
+	private bool isMoving;
 
 	// Start is called before the first frame update
 	void Start()
     {
+		animator = GetComponentInChildren<Animator>();
 		finder = FindObjectOfType<DataConverter>();
 		UpdateRoute();
     }
 
 	private void UpdateRoute() {
 		route = finder.GetRoute(transform.position, EndLocation);
-		CurrentNode = 0;
+		if (route.Count > 0) {
+			CurrentNode = 0;
+			isMoving = true;
+		}
 	}
 
 	private void Update() {
 		//Call this by interval system, probably yield the call to an interval distributer?
 		//UpdateRoute
-		MoveToward(GetCurrentTarget());
+		if(isMoving)
+			MoveToward(GetCurrentTarget());
 	}
 
 	private Vector3 GetCurrentTarget() {
@@ -35,14 +42,26 @@ public class Pathfinder : MonoBehaviour
 
 	private void MoveToward(Vector3 target) {
 		//Linear interpolation?
-		Vector3 movement = target - transform.localPosition;
-		transform.Translate( movement.normalized* MoveSpeed * Time.deltaTime);
-		//TODO: Rotate Model toward target
-		//TODO: Set the threashold so that it will not jiggle around next point forever
+		transform.LookAt(target);
+		//Vector3 movement = target - transform.localPosition;
+		//transform.Translate( movement.normalized* MoveSpeed * Time.deltaTime);
+		//Set the threashold so that it will not jiggle around next point forever
+		float d = Vector3.Distance(transform.position, target);
+		if(d < MoveSpeed * Time.deltaTime) {
+			transform.position = target;
+			++CurrentNode;
+			if (CurrentNode >= route.Count)
+				EndEvent();
+		} else {
+			transform.Translate(transform.forward * MoveSpeed * Time.deltaTime, Space.World);
+			animator.SetFloat("Speed", MoveSpeed);
+		}
 	}
 
 	private void EndEvent() {
-		//TODO: This will be called when finder reaches the end point.
+		//This will be called when finder reaches the end point.
 		//Probably play exit animation and then delete itself
+		isMoving = false;
+		animator.SetFloat("Speed", 0);
 	}
 }
